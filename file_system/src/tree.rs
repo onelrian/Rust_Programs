71599896a090c66ee78f0a65d2bee42213036393
onelrian <onelsob57@gmail.com>
2025-03-10 01:54:12 +0100

    CI: added a 'tree display' functionality

--- file_system/src/tree.rs
+++ file_system/src/tree.rs
@@ -1,17 +1,10 @@
-use crate::{traits::filesystem::FileSystemItem, types::folder::Folder};
+use crate::types::folder::Folder;
 
 fn display_tree(folder: &Folder, depth: usize) {
     let indent = "  ".repeat(depth);
     println!("{}Folder: {}", indent, folder.get_name());
 
     for item in folder.get_items() {
-        match item.as_ref() {
-            FileSystemItem::File(file) => {
-                println!("{}  File: {} (Size: {} bytes)", indent, file.name, file.size);
-            }
-            FileSystemItem::Folder(subfolder) => {
-                display_tree(subfolder, depth + 1);
-            }
-        }
+        item.display_with_indent(depth + 1);
     }
 }
