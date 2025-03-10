d144e1d43c3882b4cd4283a8d0b84cfde8a03f28
onelrian <onelsob57@gmail.com>
2025-03-10 07:46:16 +0100

    removed tree and testing

--- testing/target/debug/deps/testing-5fa94865d603ebc9.d
+++
@@ -1,5 +0,0 @@
-/home/sob/Projects/Rust/testing/target/debug/deps/testing-5fa94865d603ebc9: src/main.rs
-
-/home/sob/Projects/Rust/testing/target/debug/deps/testing-5fa94865d603ebc9.d: src/main.rs
-
-src/main.rs:
--- testing/target/debug/deps/testing-ac83bbe39d4fd9b6.d
+++
@@ -1,5 +0,0 @@
-/home/sob/Projects/Rust/testing/target/debug/deps/libtesting-ac83bbe39d4fd9b6.rmeta: src/main.rs
-
-/home/sob/Projects/Rust/testing/target/debug/deps/testing-ac83bbe39d4fd9b6.d: src/main.rs
-
-src/main.rs:
